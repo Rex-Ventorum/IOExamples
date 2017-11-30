57b4dc5390f66dc8a9b0e9a48b4d87f7ebe7c5db
L117student <L117student@L117-18-043663.bitnet.wctc.edu>
2017-11-29 21:37:13 -0600

    went idle

--- src/ioexamples/FileService.java
+++ src/ioexamples/FileService.java
@@ -1,18 +1,22 @@
 package ioexamples;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FileService {
     
     private BufferedReader reader; 
+    private PrintWriter writer;
     
-    public List<String> readFile(File file) throws FileNotFoundException, IOException{
+    public List<String> readFile(File file) throws FileNotFoundException, IOException, Exception{
         reader = new BufferedReader(new FileReader(file));
         List<String> lineList = new LinkedList<String>();
         
@@ -26,9 +30,28 @@ public class FileService {
             throw fnf;
         }catch(IOException ioe){
             throw ioe;
+        }catch(Exception e){
+            throw e;
         }finally{
             if(reader != null) reader.close();
         }
         return lineList;
+    }
+    
+    public void writeFile(File file, List<String> data, boolean append) throws IOException,FileNotFoundException,Exception {
+        try{
+            writer = new PrintWriter(new BufferedWriter(new FileWriter(file,append)));
+            for(String s : data){
+                writer.println(s);
+            }
+        }catch(FileNotFoundException fnf){
+            throw fnf;
+        }catch(IOException ioe){
+            throw ioe;
+        }catch(Exception e){
+            throw e;
+        }finally{
+            if(writer != null) writer.close();
+        }
     }
 }
--- src/ioexamples/Practice1.java
+++ src/ioexamples/Practice1.java
@@ -2,14 +2,19 @@ package ioexamples;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class Practice1 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
         File file = new File("src" + File.separatorChar + "test.txt");
         FileService service = new FileService();
         List<String> lineList = service.readFile(file);
         
         for(String line : lineList)System.out.println(line);
+        
+        lineList = Arrays.asList(new String[]{"Item1", "Item2","Item3"});
+        
+        service.writeFile(file, lineList, false);
     }   
 }
