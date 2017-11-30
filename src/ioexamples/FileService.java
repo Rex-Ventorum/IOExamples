00e9c960c1287e9d08683f317abac2e7002d2977
L117student <L117student@L117-18-043663.bitnet.wctc.edu>
2017-11-29 20:34:03 -0600

    Commit with Practrice1

---
+++ src/ioexamples/FileService.java
@@ -0,0 +1,34 @@
+package ioexamples;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FileService {
+    private File file;
+    private BufferedReader reader; 
+    
+    
+    public List<String> readFile(File file) throws FileNotFoundException, IOException{
+        reader = new BufferedReader(new FileReader(file));
+        List<String> lineList = new LinkedList<String>();
+        
+        try{
+            String line = reader.readLine();
+            while(line != null){
+                lineList.add(line);
+                line = reader.readLine();
+            }
+        }catch(FileNotFoundException fnf){
+            throw fnf;
+        }catch(IOException ioe){
+            throw ioe;
+        }finally{
+            if(reader != null) reader.close();
+        }
+    }
+}
---
+++ src/ioexamples/Practice1.java
@@ -0,0 +1,7 @@
+package ioexamples;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        
+    }   
+}
